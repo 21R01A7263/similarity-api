42a83ce8110206a3d0cdbac32ddfa0e934bcf57c
21R01A7263 <21r01a7263@cmritonline.ac.in>
2024-10-07 23:39:30 +0530

    update route

--- src/controllers/redirect.js
+++ src/controllers/redirect.js
@@ -1,5 +1,7 @@
 // const youtubeService = require("../services/youtubeService");
 
+const path = require('path');
+
 function redirect(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 }
