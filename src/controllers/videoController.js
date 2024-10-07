84cf5498bef9ce70e7f060ceb09f8d45fdfd2bbe
21R01A7263 <21r01a7263@cmritonline.ac.in>
2024-10-07 23:31:29 +0530

    npm

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -26,6 +26,10 @@ async function getSimilarVideos(req, res) {
   }
 }
 
+function redirect(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+}
+
 function extractVideoId(url) {
   const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
   const match = url.match(regExp);
